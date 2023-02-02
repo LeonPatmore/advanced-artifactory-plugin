616eefb334d78aaccbb6507c52d1694327a56589
Leon Patmore <leon.patmore@bath.edu>
2023-02-02 21:56:30 +0000

    Create AdvancedArtifactoryPlugin.kt

---
+++ advanced-artifactory-plugin-kotlin/src/main/kotlin/leon/patmore/AdvancedArtifactoryPlugin.kt
@@ -0,0 +1,12 @@
+package leon.patmore
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AdvancedArtifactoryPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+
+}
