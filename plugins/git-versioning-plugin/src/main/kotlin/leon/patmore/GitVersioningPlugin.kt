ce7d6accf81fbeee94ddf2712b0319b62d5058a4
Leon Patmore <leon.patmore@bath.edu>
2023-02-26 21:03:51 +0000

    wip

--- plugins/git-versioning-plugin/src/main/kotlin/leon/patmore/GitVersioningPlugin.kt
+++ plugins/git-versioning-plugin/src/main/kotlin/leon/patmore/GitVersioningPlugin.kt
@@ -1,12 +1,15 @@
 package leon.patmore
 
+import org.ajoberstar.grgit.gradle.GrgitService
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class GitVersioningPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
-        target.version = "abc123"
+        target.pluginManager.apply("org.ajoberstar.grgit")
+        val gitService = target.objects.property(GrgitService::class.java).get().grgit
+        target.version = gitService.head().id
     }
 
 }
